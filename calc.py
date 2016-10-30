e6e8dfa5e594b85571fade2b30b82f31e518a87c
Mahesh Uligade <maheshuligade@users.noreply.github.com>
2016-10-30 11:15:44 +0530

    Added Explanation for the Functions
    Added Explanation for the Functions

--- calc.py
+++ calc.py
@@ -4,10 +4,10 @@ from math import factorial as fact
 # def fact(n):
 	# return factorial(n)
 
-
+# Formula for calculating the Combination 
 def nCr(n, r):
 	return fact(n) // (fact(r) * fact(n - r))
 
-
+# Formula for calculating the Permutation 
 def nPr(n, r):
 	return fact(n) // fact(n - r)
